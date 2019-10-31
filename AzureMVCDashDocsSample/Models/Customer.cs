d2da5c3ad02540eb7fc8d3de8ffadfb131f076f8
Talal Shoaib <tshoaib@bilsi.com>
2019-10-31 16:01:27 -0400

    Customer.cs modified

--- AzureMVCDashDocsSample/Models/Customer.cs
+++ AzureMVCDashDocsSample/Models/Customer.cs
@@ -11,6 +11,6 @@ namespace AzureMVCDashDocsSample.Models
         [Key]
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<User> Users { get; set; }  
     }
 }
